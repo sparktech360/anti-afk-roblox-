320487483146ebc4dd533391c76578fadd51b8d7
abdullah tahir <122285747+sparktech360@users.noreply.github.com>
2025-09-12 10:55:16 +0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/sparktech360/raw-anti-afk-roblox-/refs/heads/main/anti%20afk.lua?token=GHSAT0AAAAAADLAN7CUMGIANIIFTWRIZFYI2GDWVBQ'),true))()
